9d9943f3909cf587a4f24d156fbe6b5cddf6dc5e
Gubbin1 <gubbin1@hotmail.com>
2022-08-22 21:46:27 +0900

    Checks for cells with one possibility, colors them green

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from kivy.lang import Builder
 
 sudoku_toggles = []
 selection_buttons = []
+cell_data = [[None for i in range(9)] for j in range(9)]
 
 class MainScreen(BoxLayout):
     def switch_active(self, togglebutton):
@@ -25,21 +26,29 @@ class MainScreen(BoxLayout):
         else:
             for number in selection_buttons:
                 number.disabled = True
-
-        print(tb, tb.state, tb.text)
-    
     
 
 class SudokuButtons(GridLayout):
     def __init__(self, **kwargs):
         super(SudokuButtons, self).__init__(**kwargs)
 
-        for i in range(0, 81):
-            size = dp(35)
-            b = ToggleButton(size_hint=(None, None), size=(size, size))
-            sudoku_toggles.append(b)
-            self.add_widget(b)
+        for i in range(0, 9):
+            for j in range(0, 9):
+                size = dp(35)
+                b = ToggleButton(size_hint=(None, None), size=(size, size))
+                sudoku_toggles.append(b)
+                self.add_widget(b)
+                newCell = cell(i, j, b)
+                cell_data[i][j] = newCell
 
+class EntryLayout(BoxLayout):
+    def run_solver(self, button):
+        for i in range(9):
+            for j in range(9):
+                if cell_data[i][j].entryButton.text != "":
+                    cell_data[i][j].value = cell_data[i][j].entryButton.text
+                    cell_data[i][j].poss.clear()
+                    cell_data[i][j].updateRelated()
 
 
 
@@ -66,7 +75,42 @@ class NumberEntry(GridLayout):
                 return
         for butt in selection_buttons:
             butt.disabled = True
-    
+
+class cell():
+    def __init__(self, myRow, myColumn, myButton):
+        self.row = myRow
+        self.column = myColumn
+        self.block = None
+        self.poss = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        self.value = None
+        self.entryButton = myButton
+        if 0 < self.row < 3 and 0 < self.column < 3:
+            self.block = "tl"
+        elif 0 < self.row < 3 and 2 < self.column < 6:
+            self.block = "tm"
+        elif 0 < self.row < 3 and 5 < self.column < 9:
+            self.block = "tr"
+        elif 2 < self.row < 6 and 0 < self.column < 3:
+            self.block = "ml"
+        elif 2 < self.row < 6 and 2 < self.column < 6:
+            self.block = "mm"
+        elif 2 < self.row < 6 and 5 < self.column < 9:
+            self.block = "mr"
+        elif 5 < self.row < 9 and 0 < self.column < 3:
+            self.block = "bl"
+        elif 5 < self.row < 9 and 2 < self.column < 6:
+            self.block = "bm"
+        elif 5 < self.row < 9 and 5 < self.column < 9:
+            self.block = "br"
+
+    def updateRelated(self):
+        for i in range(9):
+            for j in range(9):
+                if cell_data[i][j].row == self.row and cell_data[i][j].column == self.column:
+                    pass
+                elif cell_data[i][j].row == self.row or cell_data[i][j].column == self.column or cell_data[i][j].block == self.block:
+                    cell_data[i][j].poss.remove(self.value)
+
     
 
 class SudokuPartnerApp(App):
