81d5e6b170d70f35ea3d38325cdadb524f71c606
Gubbin1 <gubbin1@hotmail.com>
2022-08-22 17:11:49 +0900

    Enabled manual entry into UI

--- main.py
+++ main.py
@@ -9,11 +9,26 @@ from kivy.uix.widget import Widget
 from kivy.lang import Builder
 
 sudoku_toggles = []
+selection_buttons = []
 
 class MainScreen(BoxLayout):
     def switch_active(self, togglebutton):
         tb = togglebutton
+        for button in sudoku_toggles:
+            if tb == button:
+                pass
+            else:
+                button.state = "normal"
+        if tb.state == "down":
+            for number in selection_buttons:
+                number.disabled = False
+        else:
+            for number in selection_buttons:
+                number.disabled = True
+
         print(tb, tb.state, tb.text)
+    
+    
 
 class SudokuButtons(GridLayout):
     def __init__(self, **kwargs):
@@ -21,9 +36,12 @@ class SudokuButtons(GridLayout):
 
         for i in range(0, 81):
             size = dp(35)
-            b = ToggleButton(text=str(i), size_hint=(None, None), size=(size, size))
+            b = ToggleButton(size_hint=(None, None), size=(size, size))
             sudoku_toggles.append(b)
             self.add_widget(b)
+
+
+
 
 class NumberEntry(GridLayout):
     def __init__(self, **kwargs):
@@ -31,9 +49,23 @@ class NumberEntry(GridLayout):
 
         for i in range(0, 10):
             size = dp(35)
-            b = Button(text=str(i), size_hint=(None, None), size=(size, size), disabled = True)
-            sudoku_toggles.append(b)
+            title = ""
+            if i > 0:
+                title = str(i)
+            b = Button(text=title, size_hint=(None, None), size=(size, size), disabled = True, on_press = self.num_entry)
+            selection_buttons.append(b)
             self.add_widget(b)
+
+    def num_entry(self, button):
+        for cell in sudoku_toggles:
+            if cell.state == "down":
+                cell.text = button.text
+                cell.state = "normal"
+                for butt in selection_buttons:
+                    butt.disabled = True
+                return
+        for butt in selection_buttons:
+            butt.disabled = True
     
     
 
